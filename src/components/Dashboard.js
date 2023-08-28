d5eaa4c36323b23ceaec1359a080fdd052ef6517
root <root@DESKTOP-H70CEFS>
2023-08-28 10:23:20 +0200

    testing dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,58 @@
+import { Box, Card, CardContent, Divider, ThemeProvider, Grid } from '@mui/material';
+
+import Chatbot from './components/Chatbot';
+import GruvboxGraph from './components/Graph';
+import { gruvboxTheme } from './theme/Theme';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard" style={{ backgroundColor: gruvboxTheme.palette.background.default, height: "100vh" }}>
+      <ThemeProvider theme={gruvboxTheme}>
+        <Divider orientation='vertical' style={{ backgroundColor: '#ebdbb2' }} sx={{ position: "absolute", left: "17vw", height: "95vh", m: 3 }} />
+        <Box sx={{ position: "absolute", width: "78vw", right: 0, p: 3 }}>
+          <Grid container spacing={3}>
+
+            <Grid container item xs={12} spacing={3}>
+              <Grid item xs={4}>
+                <Card>
+                  <GruvboxGraph />
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card>
+                  <GruvboxGraph />
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card>
+                  <GruvboxGraph />
+                </Card>
+              </Grid>
+            </Grid>
+
+            <Grid container item xs={12} spacing={3}>
+              <Grid item xs={4}>
+                <Card>
+                  <GruvboxGraph />
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card>
+                  <GruvboxGraph />
+                </Card>
+              </Grid>
+              <Grid item xs={4}>
+                <Card>
+                  <GruvboxGraph />
+                </Card>
+              </Grid>
+            </Grid>
+
+          </Grid>
+        </Box>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default Dashboard;
