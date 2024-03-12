50192fb65c8bb023e59ce078a7365ffdcbfd2fda
huzmgod <tracert.hackstuff@gmail.com>
2024-03-13 00:03:31 +0100

    Remove jsx files from create-react-app due to deprecation. Will be replaced by next.js tsx/jsx files

--- src/constants/API.js
+++
@@ -1,6 +0,0 @@
-export const API_DEFAULT_URL = 'http://localhost';
-export const API_DEFAULT_PORT = '8000';
-
-export const TOKEN_ENDPOINT = `${API_DEFAULT_URL}:${API_DEFAULT_PORT}/tokens/`;
-
-export const NOTHING_RETURNED = 'Nothing returned.';
