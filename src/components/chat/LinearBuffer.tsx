f12eddcefeb9d5fd3f6e9194fc7b477b606fb2ae
huzmgod <tracert.hackstuff@gmail.com>
2024-04-01 14:44:05 +0200

    Update LinearBuffer component

--- src/components/chat/LinearBuffer.tsx
+++ src/components/chat/LinearBuffer.tsx
@@ -1,62 +1,41 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 export default function LinearBuffer() {
   const [progress, setProgress] = React.useState(0);
   const [buffer, setBuffer] = React.useState(10);
 
-  const intervalDuration = 100;
-  const totalDuration = 3000;
-  const steps = totalDuration / intervalDuration;
-
-  const calculateIncrement = (currentValue: number) => {
-    const minIncrement = 1;
-    const maxIncrement = 10;
-    let increment = Math.random() * (maxIncrement - minIncrement) + minIncrement;
-
-    if (currentValue > 80) {
-      const decelerationFactor = (100 - currentValue) / 20;
-      increment *= decelerationFactor;
-    }
-
-    return increment;
-  };
+  const progressRef = React.useRef(() => { });
+  React.useEffect(() => {
+    progressRef.current = () => {
+      if (progress > 200) {
+        setProgress(0);
+        setBuffer(10);
+      } else {
+        const diff = Math.random() * 10;
+        const diff2 = Math.random() * 10;
+        setProgress(progress + diff);
+        setBuffer(progress + diff + diff2);
+      }
+    };
+  });
 
   React.useEffect(() => {
-    let stepCounter = 0;
     const timer = setInterval(() => {
-      stepCounter += 1;
-      setProgress((prevProgress) => {
-        const increment = calculateIncrement(prevProgress);
-        const nextProgress = prevProgress + increment;
-        return nextProgress > 100 ? 100 : nextProgress;
-      });
-      setBuffer((prevBuffer) => {
-        const bufferIncrement = calculateIncrement(prevBuffer);
-        const nextBuffer = prevBuffer + bufferIncrement;
-        return nextBuffer > 100 ? 100 : nextBuffer;
-      });
-
-      if (stepCounter >= steps) {
-        clearInterval(timer);
-      }
-    }, intervalDuration);
+      progressRef.current();
+    }, 120);
 
     return () => {
       clearInterval(timer);
-      if (progress >= 100) {
-        setTimeout(() => {
-          setProgress(0);
-          setBuffer(10);
-        }, intervalDuration);
-      }
     };
-  }, [progress]);
-
+  }, []);
   return (
     <Box sx={{ width: '100%' }}>
-      <LinearProgress variant="buffer" value={progress} valueBuffer={buffer} />
+      <LinearProgress variant="buffer" value={progress} valueBuffer={buffer} sx={{
+        height: '5px',
+        borderRadius: 5,
+      }} />
     </Box>
   );
 }
