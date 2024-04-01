419177fe8869ff14639b0f3ed8d6b80c67cb6197
huzmgod <tracert.hackstuff@gmail.com>
2024-04-01 16:28:01 +0200

    Refactor ChatbotWebsocket component in Home page

--- src/app/websocket/page.tsx
+++ src/app/websocket/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import Chatbot from "@/components/chat/Chatbot";
 import { useChatbotDefaultTheme, ChatbotWebsocket } from "../chatbot";
-import ApiTextParser from "@/components/chat/ApiTextParser";
-import MuiTable from "@/components/MuiTable";
 
 export default function Home() {
+
+  function handleApiResponseCode(statusCode: number) {
+    console.log(`API response status code: ${statusCode}`);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24"
@@ -15,23 +16,23 @@ export default function Home() {
         height: '100vh',
         backgroundColor: '#080808'
       }}>
-        <ChatbotWebsocket
-          apiConfig={{
-            isWebsocket: true,
-            auth: false,
-            tokenName: "userToken",
-            fetchFunction: "",
-            apiQueryEndpoint: "ws://localhost:8000/chatws",
-            queryParams: {
-              'type' : 'type',
-              'data' : 'result_data',
-              'text' : 'body'
-            },
-          }}
-          themeConfig={useChatbotDefaultTheme}
-          messageRenderFunction={(text: string) => <ApiTextParser text={text} />}
-          dataRenderFunction={(data: any) => <MuiTable data={data[0]} />}
-        />
+      <ChatbotWebsocket
+        apiConfig={{
+          isWebsocket: true,
+          auth: false,
+          tokenName: "userToken",
+          fetchFunction: "",
+          apiQueryEndpoint: "ws://localhost:8000/chatws",
+          queryParams: {
+            'type': 'type',
+            'data': 'result_data',
+            'text': 'body'
+          },
+        }}
+        themeConfig={useChatbotDefaultTheme}
+        onApiResponseCode={handleApiResponseCode}
+        errorRenderFunction={(error: string) => <div>{error}</div>}
+      />
 
     </main >
   );
