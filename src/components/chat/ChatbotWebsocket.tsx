4632eaae2479837547034ca767b66c7cfb35455f
huzmgod <tracert.hackstuff@gmail.com>
2024-04-07 13:44:24 +0200

    Remove unused imports and interfaces (now imported from /types)

--- src/components/chat/ChatbotWebsocket.tsx
+++ src/components/chat/ChatbotWebsocket.tsx
@@ -1,7 +1,4 @@
 'use client';
-import '@fontsource-variable/exo-2';
-import '@fontsource-variable/fira-code';
-import '@fontsource/source-sans-pro';
 import { ArrowUpward } from '@mui/icons-material';
 import {
   Avatar,
@@ -20,109 +17,8 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { DNA } from 'react-loader-spinner';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import LinearBuffer from './LinearBuffer';
-
-interface ChatbotProps {
-  className?: string;
-  style?: React.CSSProperties;
-  apiConfig: APIConfig;
-  themeConfig: ThemeConfig;
-  onApiResponseCode?: (bool: boolean) => void;
-  messageRenderFunction?: (text: string) => JSX.Element;
-  dataRenderFunction?: (data: any) => JSX.Element;
-  graphicalDataRenderFunction?: (data: any) => JSX.Element;
-  referenceRenderFunction?: (reference: any) => JSX.Element;
-  relatedQuestionsRenderFunction?: (relatedQuestions: any) => JSX.Element;
-  errorRenderFunction?: (error: any) => JSX.Element;
-}
-
-interface APIConfig {
-  isWebsocket: boolean;
-  auth?: boolean;
-  tokenName?: string;
-  fetchFunction?: string;
-  apiQueryEndpoint: string;
-  queryParams?: Record<string, any>;
-}
-
-interface PaletteColor {
-  main: string;
-}
-
-interface Palette {
-  primary: PaletteColor;
-  secondary: PaletteColor;
-  error: PaletteColor;
-  warning: PaletteColor;
-  info: PaletteColor;
-  success: PaletteColor;
-  background: { default: string; paper: string };
-  text: { primary: string; secondary: string };
-}
-
-interface Typography {
-  fontFamily: string;
-}
-
-interface ComponentStyle extends React.CSSProperties {
-  '& img'?: React.CSSProperties;
-  '& label'?: React.CSSProperties;
-  '& label.Mui-focused'?: React.CSSProperties;
-  '& .MuiInput-underline:after'?: React.CSSProperties;
-  '& .MuiOutlinedInput-root'?: {
-    '& fieldset'?: React.CSSProperties;
-    '&:hover fieldset'?: React.CSSProperties;
-    '&.Mui-focused fieldset'?: React.CSSProperties;
-  };
-  '&::-webkit-scrollbar'?: React.CSSProperties;
-  '&::-webkit-scrollbar-track'?: React.CSSProperties;
-  '&::-webkit-scrollbar-thumb'?: React.CSSProperties;
-}
-
-interface LoaderConfig {
-  color: string;
-  backgroundColor: string;
-}
-
-interface ComponentConfig {
-  style?: ComponentStyle;
-  label?: string;
-  fullWidth?: boolean;
-  hoverBackgroundColor?: string;
-  appears?: boolean;
-  text?: string;
-  botAvatarUrl?: string;
-  userAvatarUrl?: string;
-  loader?: LoaderConfig;
-}
-
-interface Components {
-  ChatBox?: ComponentConfig;
-  LowPartBox?: ComponentConfig;
-  TextField?: ComponentConfig;
-  Button?: ComponentConfig;
-  Disclaimer?: ComponentConfig;
-  MessageBubbleBot?: ComponentConfig;
-  LoaderBot?: ComponentConfig;
-  MessageBubbleUser?: ComponentConfig;
-  Avatar?: ComponentConfig;
-  Divider?: ComponentConfig;
-}
-
-interface ThemeConfig {
-  style: React.CSSProperties;
-  palette: Palette;
-  typography: Typography;
-  components: Components;
-}
-
-interface Message {
-  text: string;
-  user: string;
-  loading?: boolean;
-  graphData?: any;
-  relatedQuestions?: any;
-  reference?: any;
-}
+import { ChatbotProps } from '@/types/chatbot';
+import { Message } from '@/types/message';
 
 const ChatbotWebsocket: React.FC<ChatbotProps> = ({
   className,
@@ -370,9 +266,8 @@ const ChatbotWebsocket: React.FC<ChatbotProps> = ({
             ...themeConfig.components?.ChatBox?.style,
           }}
         >
-          {connectionStatus === 'Closed' && (
-            <div>{ErrorRender('Connection is closed. Please refresh the page.')}</div>
-          )}
+          {connectionStatus === 'Closed' &&
+            ErrorRender('Connection is closed. Please refresh the page.')}
           <List sx={{ maxWidth: isMobile ? '100%' : '70%', margin: '0 auto' }}>
             {messages.map((message, index) => (
               <ListItem
