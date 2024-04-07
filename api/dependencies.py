b86d5813d7f28601ba1a72fdd8cd2cfd3224eeec
huzmgod <tracert.hackstuff@gmail.com>
2024-04-07 19:12:39 +0200

    Add random message functionality to ConnectionManager

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,5 +1,5 @@
 from fastapi import WebSocket
-from .utils.phrases import get_random_phrase
+from .utils.phrases import get_random_phrase, generate_related_questions, generate_reference
 import asyncio
 import json
 
@@ -20,5 +20,20 @@ class ConnectionManager:
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
+
+    async def send_random_messages(self, websocket: WebSocket):
+        response_type = "result_data"
+        result_data = get_random_phrase()
+        related_questions = generate_related_questions()
+        reference = generate_reference()
+
+        message = {
+            "type": response_type,
+            "result_data": result_data,
+            "body": get_random_phrase(),
+            "reference": reference,
+            "related_questions": related_questions,
+        }
+        await self.send_personal_message(message, websocket)
 
 manager = ConnectionManager()
