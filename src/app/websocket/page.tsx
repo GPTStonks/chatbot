05ca1cde81fc056af8f84119fdd02b27c744ff1c
huzmgod <tracert.hackstuff@gmail.com>
2024-04-09 19:26:03 +0200

    Refactor chatbot component call in /webSocket page

--- src/app/websocket/page.tsx
+++ src/app/websocket/page.tsx
@@ -1,45 +1,68 @@
 'use client';
-import { useChatbotDefaultTheme, ChatbotWebsocket } from '../chatbot';
+import useChatbotDefaultTheme from '@/components/chat/ChatbotDefaultTheme';
+import ChatbotWebsocket from '@/layouts/ChatbotWebsocket';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Home() {
   const [initializedChat, setInitializedChat] = React.useState(false);
+  const [chatData, setChatData] = React.useState<any>(null);
+
+  const themeConfig = useChatbotDefaultTheme;
 
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-center p-24"
       style={{
-        position: 'absolute',
-        bottom: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         height: '100vh',
+        width: '100vw',
         backgroundColor: '#080808',
       }}
     >
-      <ChatbotWebsocket
-        apiConfig={{
-          isWebsocket: true,
-          auth: false,
-          tokenName: 'userToken',
-          fetchFunction: '',
-          apiQueryEndpoint: 'ws://localhost:8000/chatws',
-          queryParams: {
-            type: 'type',
-            data: 'result_data',
-            text: 'body',
-          },
-        }}
-        themeConfig={useChatbotDefaultTheme}
-        onApiResponseCode={(bool: boolean) => {
-          setInitializedChat(bool);
-        }}
-        dataRenderFunction={(data: any) => <div>{data}</div>}
-        graphicalDataRenderFunction={(data: any) => <div>{data}</div>}
-        referenceRenderFunction={(reference: string) => <div>{reference}</div>}
-        relatedQuestionsRenderFunction={(relatedQuestions: string[]) => (
-          <div>{relatedQuestions}</div>
-        )}
-        errorRenderFunction={(error: string) => <div>{error}</div>}
-      />
+      <div style={{ width: '20vw', height: '100%' }}></div>
+      <div style={{ width: '100%', height: '100%' }}>
+        <ChatbotWebsocket
+          apiConfig={{
+            auth: false,
+            tokenName: 'userToken',
+            fetchFunction: '',
+            apiQueryEndpoint: 'ws://localhost:5000/chatws',
+            queryParams: {
+              type: 'type',
+              data: 'result_data',
+              text: 'body',
+              reference: 'reference',
+              related: 'related',
+            },
+          }}
+          themeConfig={themeConfig}
+          setDataForParent={(data: any) => {
+            setChatData(data);
+          }}
+          onApiResponseCode={(bool: boolean) => {
+            setInitializedChat(bool);
+          }}
+          botMessageRenderFunction={(text: string) => (
+            <Box
+              sx={{
+                maxWidth: '100%',
+                overflowWrap: 'break-word',
+              }}
+            >
+              <Typography
+                sx={{
+                  wordWrap: 'break-word',
+                }}
+              >
+                {text}
+              </Typography>
+            </Box>
+          )}
+        />
+      </div>
+      <div style={{ width: '20vw', height: '100%' }}></div>
     </main>
   );
 }
