afab2ec5ba05de276310b3d3f077da6d237fc7a0
huzmgod <tracert.hackstuff@gmail.com>
2024-07-18 17:31:56 +0200

    feat: Add subqueryRenderFunction and modelStepTypes to ChatbotProps interface

--- src/components/chat/ChatbotCore.tsx
+++ src/components/chat/ChatbotCore.tsx
@@ -1,13 +1,15 @@
-import { ThemeConfig } from '@/types/chatbot';
+import { APIConfig, ThemeConfig } from '@/types/chatbot';
 import { Message } from '@/types/message';
 import { Avatar, Box, List, ListItem, Typography } from '@mui/material';
 import React, { useCallback } from 'react';
 import { DNA } from 'react-loader-spinner';
 import LinearBuffer from './LinearBuffer';
+import RenderFunctions from '../renderers/RenderFunctions';
 
 const ChatbotCore = ({
   messages,
   themeConfig,
+  apiConfig,
   isMobile,
   botUser,
   humanUser,
@@ -23,10 +25,12 @@ const ChatbotCore = ({
   providerRenderFunction,
   referenceRenderFunction,
   relatedQuestionsRenderFunction,
+  subqueryRenderFunction,
 }: {
   messages: Message[];
   themeConfig: ThemeConfig;
   isMobile: boolean;
+  apiConfig: APIConfig;
   botUser: string;
   humanUser: string;
   botMessage: any;
@@ -50,98 +54,43 @@ const ChatbotCore = ({
     relatedQuestions: any,
     sendCustomMessage: (message: string) => void,
   ) => JSX.Element | null;
+  subqueryRenderFunction: (
+    subqueryQuestion: string[],
+    subqueryResponse: string[],
+  ) => JSX.Element | null;
 }) => {
-  const WelcomeMessageRender = useCallback(
-    (sendCustomMessage: (message: string) => void) => {
-      return welcomeMessageRenderFunction ? (
-        welcomeMessageRenderFunction(sendCustomMessage)
-      ) : (
-        <Box
-          sx={{
-            position: 'fixed',
-            width: '100vw',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            textAlign: 'center',
-          }}
-        >
-          <Typography
-            variant="h4"
-            sx={{
-              color: 'white',
-            }}
-          >
-            Welcome! How can I help you today?
-          </Typography>
-        </Box>
-      );
-    },
-    [welcomeMessageRenderFunction],
-  );
+  const {
+    WelcomeMessageRender,
+    BotMessageRender,
+    UserMessageRender,
+    DataRender,
+    ProviderRender,
+    ReferenceRender,
+    RelatedQuestionsRender,
+    SubqueryRender,
+  } = RenderFunctions({
+    welcomeMessageRenderFunction,
+    botMessageRenderFunction,
+    userMessageRenderFunction,
+    dataRenderFunction,
+    providerRenderFunction,
+    referenceRenderFunction,
+    relatedQuestionsRenderFunction,
+    subqueryRenderFunction,
+    sendCustomMessage,
+  });
 
-  const BotMessageRender = useCallback(
-    (message: any, input: string) => {
-      return botMessageRenderFunction ? (
-        botMessageRenderFunction(message, input)
-      ) : (
-        <Typography>{message.text}</Typography>
-      );
-    },
-    [botMessageRenderFunction],
-  );
+  const getMessage = (text: string) => {
+    if (apiConfig?.modelStepTypes) {
+      const modelStepTypes = apiConfig.modelStepTypes;
 
-  const UserMessageRender = useCallback(
-    (text: string) => {
-      return userMessageRenderFunction ? (
-        userMessageRenderFunction(text)
-      ) : (
-        <Typography
-          variant="h4"
-          sx={{
-            overflow: 'hidden',
-            display: '-webkit-box',
-            WebkitLineClamp: 3,
-            WebkitBoxOrient: 'vertical',
-            textOverflow: 'ellipsis',
-          }}
-        >
-          {text}
-        </Typography>
-      );
-    },
-    [userMessageRenderFunction],
-  );
+      if (modelStepTypes.hasOwnProperty(text)) {
+        return modelStepTypes[text];
+      }
+    }
 
-  const DataRender = useCallback(
-    (data: any) => {
-      return dataRenderFunction ? dataRenderFunction(data) : null;
-    },
-    [dataRenderFunction],
-  );
-
-  const ProviderRender = useCallback(
-    (providers: any) => {
-      return providerRenderFunction ? providerRenderFunction(providers) : null;
-    },
-    [providerRenderFunction],
-  );
-
-  const ReferenceRender = useCallback(
-    (reference: any) => {
-      return referenceRenderFunction ? referenceRenderFunction(reference) : null;
-    },
-    [referenceRenderFunction],
-  );
-
-  const RelatedQuestionsRender = useCallback(
-    (relatedQuestions: any, sendCustomMessage: (message: string) => void) => {
-      return relatedQuestionsRenderFunction
-        ? relatedQuestionsRenderFunction(relatedQuestions, sendCustomMessage)
-        : null;
-    },
-    [relatedQuestionsRenderFunction],
-  );
+    return `Preparing content from ${text}...`;
+  };
 
   return (
     <Box
@@ -261,6 +210,15 @@ const ChatbotCore = ({
                       display: 'flex',
                     }}
                   >
+                    {message.subqueryQuestion &&
+                      message.subqueryResponse &&
+                      SubqueryRender(message.subqueryQuestion, message.subqueryResponse)}
+                  </Box>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                    }}
+                  >
                     {message.providers && ProviderRender(message.providers)}
                   </Box>
                   <Box
@@ -317,13 +275,7 @@ const ChatbotCore = ({
                   marginLeft: '1rem',
                 }}
               >
-                {botMessage.text == 'world_knowledge' && '🧭 Navigating the Internet...'}
-
-                {botMessage.text == 'simple_reflection' && 'Wrapping up...'}
-
-                {botMessage.text != 'world_knowledge' && botMessage.text != 'simple_reflection' && (
-                  <Typography> Preparing content from {botMessage.text}...</Typography>
-                )}
+                {getMessage(botMessage.text)}
               </Typography>
             </Box>
           </ListItem>
